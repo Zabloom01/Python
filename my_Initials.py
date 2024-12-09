4bfbbb450bf7e3303498585f65b5ac3c1d37ae8b
Zach <Zabloom01@gmail.com>
2024-12-08 23:12:13 -0500

    Create my_Initials.py

---
+++ my_Initials.py
@@ -0,0 +1,11 @@
+# My initials: ZB
+# A fun fact about yourself: I am from Alaska
+
+# Think of this as a matrix that is 7x5 and you are trying to fit each letter in those spaces
+print("zzzzz" + "   " + "BBBB ")
+print("    z" + "   " + "B   B")
+print("   z"  + "    " + "B   B")
+print("  z"   + "     " + "BBBB ")
+print(" z"    + "      " + "B   B")
+print("z"     + "       " + "B   B")
+print("zzzzz" + "   " + "BBBB ")
